e8044a4b484b27b0c505e926abdee01cc628f20b
Nightmayr <umayr23@gmail.com>
2019-11-18 21:42:10 +0000

    configured seed job to checkout library and load dsl scripts

--- resources/init/seedJob.groovy
+++ resources/init/seedJob.groovy
@@ -1,5 +1,10 @@
 node('master'){
-    stage("test"){
-        println("hello jenkins")
+    stage("checkout"){
+       cleanWs()
+       checkout scm
+    }
+
+    stage("Job seeding"){
+        jobDsl(targets: 'resources/jobDSL/*.groovy')
     }
 }
